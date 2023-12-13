2010657a53df1b7f5d3202c07db205ce19eca45a
Mark J Koch <maehem@users.noreply.github.com>
2023-12-12 18:10:24 -0800

    Formatting and added a note about Signal

--- src/main/java/com/maehem/mangocad/model/element/highlevel/Signal.java
+++ src/main/java/com/maehem/mangocad/model/element/highlevel/Signal.java
@@ -1,17 +1,17 @@
 /*
-    Licensed to the Apache Software Foundation (ASF) under one or more 
+    Licensed to the Apache Software Foundation (ASF) under one or more
     contributor license agreements.  See the NOTICE file distributed with this
-    work for additional information regarding copyright ownership.  The ASF 
-    licenses this file to you under the Apache License, Version 2.0 
-    (the "License"); you may not use this file except in compliance with the 
+    work for additional information regarding copyright ownership.  The ASF
+    licenses this file to you under the Apache License, Version 2.0
+    (the "License"); you may not use this file except in compliance with the
     License.  You may obtain a copy of the License at
 
       http://www.apache.org/licenses/LICENSE-2.0
 
-    Unless required by applicable law or agreed to in writing, software 
-    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT 
-    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the 
-    License for the specific language governing permissions and limitations 
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+    License for the specific language governing permissions and limitations
     under the License.
  */
 package com.maehem.mangocad.model.element.highlevel;
@@ -28,7 +28,10 @@ import java.util.List;
  *      class         %Class;        "0"
  *      airwireshidden %Bool;        "no"
  * </pre>
- * 
+ *
+ * Does not use _AQuantum.layerNum! Signal sub-elements (wire, polygon, etc.)
+ * each define their own layer number.
+ * *
  * @author Mark J Koch ( @maehem on GitHub )
  */
 public class Signal extends _AQuantum {
@@ -38,9 +41,9 @@ public class Signal extends _AQuantum {
     private String name;
     private int netClass = 0;
     private boolean airwiresHidden = false;
-    
+
     private final ArrayList<_AQuantum> elements = new ArrayList<>();
-    
+
 
     @Override
     public String getElementName() {
