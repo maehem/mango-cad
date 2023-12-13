3b5f443a4973d6a15b5e89a002e7943a56e1f398
Mark J Koch <maehem@users.noreply.github.com>
2023-12-12 19:10:35 -0800

    Added render of signals and corrected some layer colors.

--- src/main/java/com/maehem/mangocad/view/board/BoardPreview.java
+++ src/main/java/com/maehem/mangocad/view/board/BoardPreview.java
@@ -1,34 +1,39 @@
 /*
-    Licensed to the Apache Software Foundation (ASF) under one or more 
+    Licensed to the Apache Software Foundation (ASF) under one or more
     contributor license agreements.  See the NOTICE file distributed with this
-    work for additional information regarding copyright ownership.  The ASF 
-    licenses this file to you under the Apache License, Version 2.0 
-    (the "License"); you may not use this file except in compliance with the 
+    work for additional information regarding copyright ownership.  The ASF
+    licenses this file to you under the Apache License, Version 2.0
+    (the "License"); you may not use this file except in compliance with the
     License.  You may obtain a copy of the License at
 
       http://www.apache.org/licenses/LICENSE-2.0
 
-    Unless required by applicable law or agreed to in writing, software 
-    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT 
-    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the 
-    License for the specific language governing permissions and limitations 
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+    License for the specific language governing permissions and limitations
     under the License.
  */
 package com.maehem.mangocad.view.board;
 
+import com.maehem.mangocad.model.ColorPalette;
 import com.maehem.mangocad.model._AQuantum;
 import com.maehem.mangocad.model.element.basic.*;
 import com.maehem.mangocad.model.element.drawing.Board;
 import com.maehem.mangocad.model.element.drawing.Library;
 import com.maehem.mangocad.model.element.highlevel.Footprint;
-import com.maehem.mangocad.view.ControlPanel;
+import com.maehem.mangocad.model.element.highlevel.Signal;
+import com.maehem.mangocad.model.element.misc.LayerElement;
+import com.maehem.mangocad.view.ColorUtils;
 import com.maehem.mangocad.view.library.LibraryElementNode;
 import java.util.Optional;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javafx.collections.ObservableList;
 import javafx.scene.Group;
 import javafx.scene.Node;
 import javafx.scene.paint.Color;
+import javafx.scene.transform.Rotate;
 
 /**
  *
@@ -36,7 +41,7 @@ import javafx.scene.paint.Color;
  */
 public class BoardPreview extends Group {
 
-    private static final Logger LOGGER = Logger.getLogger(ControlPanel.class.getSimpleName());
+    public static final Logger LOGGER = Logger.getLogger("com.maehem.mangocad");
 
     private final Board board;
 
@@ -44,36 +49,72 @@ public class BoardPreview extends Group {
         super();
         this.board = board;
 
+        populateGrid();
         populateNode();
     }
 
+    private void populateGrid() {
+        // Grid
+        for (int i = 0; i < 10; i++) {
+            Wire gridline = new Wire();
+            gridline.setX1(0);
+            gridline.setY1(i * 1.27);
+            gridline.setX2(25.40);
+            gridline.setY2(i * 1.27);
+
+            gridline.setWidth(0.01);
+            getChildren().add(LibraryElementNode.createWireNode(gridline, new Color(0.2, 0.2, 0.2, 1.0), false));
+        }
+        for (int i = 0; i < 20; i++) {
+            Wire gridline = new Wire();
+            gridline.setX1(i * 1.27);
+            gridline.setY1(0);
+            gridline.setX2(i * 1.27);
+            gridline.setY2(12.70);
+
+            gridline.setWidth(0.01);
+            getChildren().add(LibraryElementNode.createWireNode(gridline, new Color(0.2, 0.2, 0.2, 1.0), false));
+        }
+
+    }
+
     private void populateNode() {
+        LayerElement[] layers = board.getParentDrawing().getLayers();
+        ColorPalette palette = board.getParentDrawing().getPalette();
+        ObservableList<Node> chld = getChildren();
 
         for (_AQuantum element : board.getPlain()) {
+            int colorIndex = layers[element.getLayerNum()].getColorIndex();
+            Color c = ColorUtils.getColor(palette.getHex(colorIndex));
+
             // polygon | wire | text | dimension | circle | spline | rectangle | frame | hole
             if (element instanceof ElementPolygon e) {
-                getChildren().add(LibraryElementNode.createPolygon(e, Color.CORAL, false));
+                chld.add(LibraryElementNode.createPolygon(e, c, false));
             } else if (element instanceof Wire e) {
-                getChildren().add(LibraryElementNode.createWireNode(e, Color.RED, false));
+                chld.add(LibraryElementNode.createWireNode(e, c, false));
             } else if (element instanceof ElementText e) {
-                getChildren().add(LibraryElementNode.createText(e, Color.CORAL));
+                chld.add(LibraryElementNode.createText(e, c));
             } else if (element instanceof Dimension e) {
-                //getChildren().add(LibraryElementNode.createDimensionNode(e, Color.RED));
-                LOGGER.log(Level.SEVERE, "TODO: Implement Dimension Node");
+                chld.add(LibraryElementNode.createDimensionNode(
+                        e, layers, palette)
+                );
             } else if (element instanceof ElementCircle e) {
-                getChildren().add(LibraryElementNode.createCircleNode(e, Color.CORAL, false));
+                chld.add(LibraryElementNode.createCircleNode(e, c, false));
             } else if (element instanceof Spline e) {
-                //getChildren().add(LibraryElementNode.createSplineNode(e, Color.CORAL));
+                // Only the plain group can have Spline and the Spline can only
+                // be on the Dimension layer.
+
+                //chld.add(LibraryElementNode.createSplineNode(e, c));
                 LOGGER.log(Level.SEVERE, "TODO: Implement Spline Node");
             } else if (element instanceof ElementRectangle e) {
-                getChildren().add(LibraryElementNode.createRectangle(e, Color.RED, false));
+                chld.add(LibraryElementNode.createRectangle(e, c, false));
             } else if (element instanceof FrameElement e) {
-                getChildren().add(LibraryElementNode.createFrameNode(e, Color.RED));
+                chld.add(LibraryElementNode.createFrameNode(e, c));
             } else if (element instanceof Hole e) {
-                //getChildren().add(LibraryElementNode.createHoleNode(e, Color.RED));
-                LOGGER.log(Level.SEVERE, "TODO: Implement Hole Node");
+                chld.add(LibraryElementNode.createHoleNode(e, c));
+            } else {
+                LOGGER.log(Level.SEVERE, "Encountered unhandled plain element: " + element.getElementName());
             }
-
         }
 
         for (ElementElement element : board.getElements()) {
@@ -85,45 +126,58 @@ public class BoardPreview extends Group {
             if (!libSearch.isEmpty()) {
                 Footprint pkg = libSearch.get().getPackage(element.getFootprint());
                 Node pkgPreview = LibraryElementNode.createPackageNode(
-                        pkg, board.getParentDrawing().getLayers(), 
+                        pkg, board.getParentDrawing().getLayers(),
                         board.getParentDrawing().getPalette()
                 );
-                //Node pkgPreview = DetailNodes.footprintPreview(pkg, libSearch.get(), false);
                 pkgPreview.setLayoutX(element.getX());
                 pkgPreview.setLayoutY(-element.getY());
-                pkgPreview.setRotate(element.getRot());
-                getChildren().add(pkgPreview);
+                pkgPreview.getTransforms().add(new Rotate(-element.getRot()));
+                chld.add(pkgPreview);
             } else {
                 LOGGER.log(Level.SEVERE,
                         "Couldn''t find local library called: {0} for package: {1}",
                         new Object[]{element.getLibrary(), element.getFootprint()}
                 );
             }
-//            board.getLibraries().forEach((lib) -> {
-//                if (lib.getName().equals(element.getLibrary())) {
-//                    Footprint pkg = lib.getPackage(element.getFootprint());
-//                    Node pkgPreview = DetailNodes.footprintPreview(pkg, lib, false);
-//                    pkgPreview.setLayoutX(element.getX());
-//                    pkgPreview.setLayoutY(-element.getY());
-//                    pkgPreview.setRotate(element.getRot());
-//                    getChildren().add(pkgPreview);
+        }
 
-//                    DeviceSet deviceSet = lib.getDeviceSet(p.getDeviceSet());
-//                    deviceSet.getGates().forEach((gate) -> {
-//                        if (element.getGate().equals(gate.getName())) {
-//                            Symbol symbol = lib.getSymbol(gate.getSymbol());
-//                            LayerElement[] layers = lib.getParentDrawing().getLayers();
-//                            ColorPalette palette = lib.getParentDrawing().getPalette();
-//
-//                            Node symbolPreview = LibraryElementNode.createSymbolNode(symbol, layers, palette);
-//                            symbolPreview.setLayoutX(element.getX());
-//                            symbolPreview.setLayoutY(-element.getY());
-//                            symbolPreview.setRotate(element.getRot());
-//                            getChildren().add(symbolPreview);
-//                        }
-//                    });
-//                }
-//            });
+        for (Signal sig : board.getSignals()) {
+            LOGGER.log(Level.FINE, "Signal: {0} with netClass: {1}",
+                    new Object[]{sig.getName(), sig.getNetClassNum()});
+            for (_AQuantum el : sig.getElements()) {
+                if (el instanceof Wire w) {
+                    int colorIndex = layers[el.getLayerNum()].getColorIndex();
+                    Color c = ColorUtils.getColor(palette.getHex(colorIndex));
+                    LOGGER.log(Level.FINER, "    Element: {0} on layerNum: {1} at: {2},{3} to {4},{5} with width: {6}",
+                            new Object[]{
+                                w.getElementName(), w.getLayerNum(),
+                                w.getX1(), w.getY1(), w.getX2(), w.getY2(),
+                                w.getWidth()
+                            });
+                    chld.add(LibraryElementNode.createWireNode(w, c, false));
+
+                } else if (el instanceof ContactRef cref) {
+
+                    LOGGER.log(Level.SEVERE, "    ContactRef not handled: {0} pad:{1} routeTag:{2}",
+                            new Object[]{
+                                el.getElementName(),
+                                cref.getPad(),
+                                cref.getRouteTag()
+                            }
+                    );
+                } else if (el instanceof Via v) {
+                    Color padColor = ColorUtils.getColor(palette.getHex(layers[18].getColorIndex()));
+                    Color maskColor = ColorUtils.getColor(palette.getHex(layers[29].getColorIndex()));
+                    chld.add(LibraryElementNode.createVia(v, padColor, maskColor));
+                    //LOGGER.log(Level.SEVERE, "Via not handled: " + el.getElementName());
+                } else if (el instanceof ElementPolygon ep) {
+                    int colorIndex = layers[el.getLayerNum()].getColorIndex();
+                    Color c = ColorUtils.getColor(palette.getHex(colorIndex));
+                    chld.add(LibraryElementNode.createPolygon(ep, c, false));
+                } else {
+                    LOGGER.log(Level.SEVERE, "    Signal Element not handled: {0}", el.getElementName());
+                }
+            }
         }
     }
 
