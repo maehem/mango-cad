d865d3bd89662d024474bfd5700ab6a78951e51f
Mark J Koch <maehem@users.noreply.github.com>
2024-03-11 22:15:30 -0700

    Auto format made aesthetic changes to comments.

--- src/main/java/com/maehem/mangocad/view/controlpanel/tab/PreviewContent.java
+++ src/main/java/com/maehem/mangocad/view/controlpanel/tab/PreviewContent.java
@@ -1,17 +1,17 @@
 /*
-    Licensed to the Apache Software Foundation (ASF) under one or more 
+    Licensed to the Apache Software Foundation (ASF) under one or more
     contributor license agreements.  See the NOTICE file distributed with this
-    work for additional information regarding copyright ownership.  The ASF 
-    licenses this file to you under the Apache License, Version 2.0 
-    (the "License"); you may not use this file except in compliance with the 
+    work for additional information regarding copyright ownership.  The ASF
+    licenses this file to you under the Apache License, Version 2.0
+    (the "License"); you may not use this file except in compliance with the
     License.  You may obtain a copy of the License at
 
       http://www.apache.org/licenses/LICENSE-2.0
 
-    Unless required by applicable law or agreed to in writing, software 
-    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT 
-    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the 
-    License for the specific language governing permissions and limitations 
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+    License for the specific language governing permissions and limitations
     under the License.
  */
 package com.maehem.mangocad.view.controlpanel.tab;
@@ -25,9 +25,9 @@ import javafx.scene.layout.BorderPane;
  * @author Mark J Koch ( @maehem on GitHub )
  */
 public class PreviewContent extends BorderPane {
-    
+
     public PreviewContent() {
-        
+
         setCenter(new TextArea("Content Area"));
         //setMaxHeight(Double.MAX_VALUE);
         //setPrefHeight(Double.MAX_VALUE);
@@ -42,9 +42,9 @@ public class PreviewContent extends BorderPane {
         //Node cNode = ControlPanelUtils.markdownNode(1.5, ControlPanelUtils.getItemDescriptionFull(item));
         setCenter(item);
     }
-    
+
     public Node getItem() {
         return getCenter();
     }
-    
+
 }
