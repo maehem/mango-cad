257e61a3e151992ed94986099d26e7917a6c69c1
Mark J Koch <maehem@users.noreply.github.com>
2024-03-15 15:58:41 -0700

    Change Home page to new release (BB) Bodacious Boobie.

--- src/main/java/com/maehem/mangocad/view/controlpanel/tab/HomeContent.java
+++ src/main/java/com/maehem/mangocad/view/controlpanel/tab/HomeContent.java
@@ -1,17 +1,17 @@
 /*
-    Licensed to the Apache Software Foundation (ASF) under one or more 
+    Licensed to the Apache Software Foundation (ASF) under one or more
     contributor license agreements.  See the NOTICE file distributed with this
-    work for additional information regarding copyright ownership.  The ASF 
-    licenses this file to you under the Apache License, Version 2.0 
-    (the "License"); you may not use this file except in compliance with the 
+    work for additional information regarding copyright ownership.  The ASF
+    licenses this file to you under the Apache License, Version 2.0
+    (the "License"); you may not use this file except in compliance with the
     License.  You may obtain a copy of the License at
 
       http://www.apache.org/licenses/LICENSE-2.0
 
-    Unless required by applicable law or agreed to in writing, software 
-    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT 
-    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the 
-    License for the specific language governing permissions and limitations 
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+    License for the specific language governing permissions and limitations
     under the License.
  */
 package com.maehem.mangocad.view.controlpanel.tab;
@@ -42,7 +42,7 @@ public class HomeContent extends BorderPane {
 //        node.getChildren().add(new Text("Mango Viewer 1.0"));
 //        node.getChildren().add(new Text("(Anxious Alicanto)"));
 
-        InputStream mdContent = HomeContent.class.getResourceAsStream("/home/AnxiousAlicanto.md");
+        InputStream mdContent = HomeContent.class.getResourceAsStream("/home/BodaciousBoobie.md");
 
         String str = "# Ooops";
         try {
---
+++ src/main/resources/home/BodaciousBoobie.md
@@ -0,0 +1,25 @@
+## Bodacious Boobie  (03/2024)
+
+![Photograph of the Blue Footed Boobie.](Boobie.jpeg)
+
+The second release of **MangoCAD**, a viewer application for ***EagleCAD*** formatted design files.
+
+### New Features:
++ Enhanced viewing of BRD files.
++ Improved text rendering with ratio(boldness), rotations and alignments.
+
+
+### Bug Fixes:
++ All kinds.
+
+### Future Release Goals:
++ Library Symbol Editor
++ Library Footprint Editor
++ Improved Markdown processor.
++ Improved Printing experience.
++ Print to PDF.
++ Bill of Materials generator.
+
+
+###### [Image by Vince Smith  ](https://commons.wikimedia.org/wiki/File:Blue-footed_Booby_(4885194032).jpg )   
+Used under Creative Commons License
