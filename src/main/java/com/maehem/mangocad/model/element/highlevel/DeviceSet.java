e0d6c1ad6fc8914dfb0a4f1819032d275a637ff6
Mark J Koch <maehem@users.noreply.github.com>
2024-03-21 22:26:30 -0700

    Add isValidName() checker. WIP.

--- src/main/java/com/maehem/mangocad/model/element/highlevel/DeviceSet.java
+++ src/main/java/com/maehem/mangocad/model/element/highlevel/DeviceSet.java
@@ -1,26 +1,26 @@
 /*
-    Licensed to the Apache Software Foundation (ASF) under one or more 
+    Licensed to the Apache Software Foundation (ASF) under one or more
     contributor license agreements.  See the NOTICE file distributed with this
-    work for additional information regarding copyright ownership.  The ASF 
-    licenses this file to you under the Apache License, Version 2.0 
-    (the "License"); you may not use this file except in compliance with the 
+    work for additional information regarding copyright ownership.  The ASF
+    licenses this file to you under the Apache License, Version 2.0
+    (the "License"); you may not use this file except in compliance with the
     License.  You may obtain a copy of the License at
 
       http://www.apache.org/licenses/LICENSE-2.0
 
-    Unless required by applicable law or agreed to in writing, software 
-    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT 
-    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the 
-    License for the specific language governing permissions and limitations 
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+    License for the specific language governing permissions and limitations
     under the License.
  */
 package com.maehem.mangocad.model.element.highlevel;
 
+import com.maehem.mangocad.model._AQuantum;
+import com.maehem.mangocad.model.element.basic.Gate;
+import com.maehem.mangocad.model.element.misc.Description;
 import java.util.ArrayList;
 import java.util.List;
-import com.maehem.mangocad.model.element.basic.Gate;
-import com.maehem.mangocad.model._AQuantum;
-import com.maehem.mangocad.model.element.misc.Description;
 
 /**
  *
@@ -43,14 +43,14 @@ public class DeviceSet extends _AQuantum {
     private String name;
     private String urn;
     private boolean locallyModified = false;
-    
-    
+
+
     private String prefix = "";
     private boolean uservalue = false;
-    
+
     private int libraryVersion = 0;
     private boolean libraryLocallyModified = false;
-    
+
     private final List<Description> descriptions = new ArrayList<>();
     private final List<Gate> gates = new ArrayList<>();
     private final List<Device> devices = new ArrayList<>();
@@ -207,7 +207,12 @@ public class DeviceSet extends _AQuantum {
     public Device lookupDevice(String devName) {
         return devices.stream().
                 filter(device -> devName.equals(device.getName())).
-                findAny().orElse(null);        
+                findAny().orElse(null);
     }
-    
+
+    public static boolean isValidName(Object nameResult) {
+        // TODO: Check for invalid things like characters in name, or name starts with a digit.
+        return true;
+    }
+
 }
