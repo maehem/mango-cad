becc69c15d3286fa6e32156ba1abe57b491825f5
Mark J Koch <maehem@users.noreply.github.com>
2024-04-10 10:23:53 -0700

    Move Command to sub package.

--- src/main/java/com/maehem/mangocad/controller/command/Command.java
+++ src/main/java/com/maehem/mangocad/controller/command/Command.java
@@ -14,7 +14,9 @@
     License for the specific language governing permissions and limitations
     under the License.
  */
-package com.maehem.mangocad.controller;
+package com.maehem.mangocad.controller.command;
+
+import com.maehem.mangocad.controller.Memento;
 
 /**
  *
