dd8072c2af82e8e41a8138817b3fde7ac827789e
Mark J Koch <maehem@users.noreply.github.com>
2024-04-10 10:52:42 -0700

    Add static helper methods.

--- src/main/java/com/maehem/mangocad/controller/command/Command.java
+++ src/main/java/com/maehem/mangocad/controller/command/Command.java
@@ -17,6 +17,12 @@
 package com.maehem.mangocad.controller.command;
 
 import com.maehem.mangocad.controller.Memento;
+import com.maehem.mangocad.controller.command.exception.CommandException;
+import com.maehem.mangocad.controller.command.exception.PinCommandException;
+import static com.maehem.mangocad.view.ControlPanel.LOGGER;
+import java.util.List;
+import java.util.ListIterator;
+import java.util.logging.Level;
 
 /**
  *
@@ -41,4 +47,61 @@ public abstract class Command {
     public abstract void execute();
 
     public abstract void unExecute();
+
+    protected static String extractPinName(List<String> argList) throws CommandException {
+        String pinName = null;
+        // Find 'NAME' item in list or fail. Could be anywhere.
+        ListIterator<String> argIter = argList.listIterator();
+        while (argIter.hasNext()) {
+            String arg = argIter.next();
+            if (arg.startsWith("'") && arg.endsWith("'")) {
+                pinName = arg.replaceAll("'", "");
+                argIter.remove();
+                break;
+            }
+        }
+
+        return pinName;
+    }
+
+    protected static String extractCoords(List<String> argList) throws CommandException {
+        // Find any args that are coordinates ==>  (xxx ... yyy)
+        // Could be anywhere.
+        String coordString = null;
+
+        ListIterator<String> iter = argList.listIterator();
+
+        boolean coordHasEnd = false;
+        while (iter.hasNext()) {
+            String arg = iter.next();
+            if (coordString != null) {
+                if (arg.endsWith(")")) {
+                    coordString += " " + arg.substring(0, arg.lastIndexOf(")"));
+                    LOGGER.log(Level.SEVERE, "Found final coordinate: ({0})", coordString);
+                    iter.remove();
+                    coordHasEnd = true;
+                    break;
+                } else {
+                    coordString += " " + arg;
+                    LOGGER.log(Level.SEVERE, "Found coordinate chunk: {0}", arg);
+                    iter.remove();
+                }
+            } else if (arg.startsWith("(")) {
+                coordString = arg.substring(1);
+                LOGGER.log(Level.SEVERE, "Found start of coord: {0}", arg);
+                if (arg.endsWith(")")) {
+                    coordString = coordString.substring(0, coordString.lastIndexOf(")"));
+                    coordHasEnd = true;
+                    iter.remove();
+                    break;
+                }
+                iter.remove();
+            } // else ignore the arg
+        }
+        if (coordString != null && !coordHasEnd) {
+            throw new PinCommandException("Coordinate had no end braces!");
+        }
+
+        return coordString;
+    }
 }
