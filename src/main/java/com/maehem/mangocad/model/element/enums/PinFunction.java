42cc68cbc00efa44d7d3f16d8f93924c2df71d50
Mark J Koch <maehem@users.noreply.github.com>
2024-04-09 18:53:05 -0700

    Add command key mnemonic. Minor re-format.

--- src/main/java/com/maehem/mangocad/model/element/enums/PinFunction.java
+++ src/main/java/com/maehem/mangocad/model/element/enums/PinFunction.java
@@ -1,17 +1,17 @@
 /*
-    Licensed to the Apache Software Foundation (ASF) under one or more 
+    Licensed to the Apache Software Foundation (ASF) under one or more
     contributor license agreements.  See the NOTICE file distributed with this
-    work for additional information regarding copyright ownership.  The ASF 
-    licenses this file to you under the Apache License, Version 2.0 
-    (the "License"); you may not use this file except in compliance with the 
+    work for additional information regarding copyright ownership.  The ASF
+    licenses this file to you under the Apache License, Version 2.0
+    (the "License"); you may not use this file except in compliance with the
     License.  You may obtain a copy of the License at
 
       http://www.apache.org/licenses/LICENSE-2.0
 
-    Unless required by applicable law or agreed to in writing, software 
-    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT 
-    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the 
-    License for the specific language governing permissions and limitations 
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+    License for the specific language governing permissions and limitations
     under the License.
  */
 package com.maehem.mangocad.model.element.enums;
@@ -23,17 +23,19 @@ package com.maehem.mangocad.model.element.enums;
  */
 public enum PinFunction {
     NONE("none"), DOT("dot"), CLK("clk"), DOTCLK("dotclk");
-    
+
     private final String code;
- 
+
+    public static final String COMMAND_KEY = "function";
+
     private PinFunction(String code) {
         this.code = code;
     }
- 
+
     public String code() {
         return code;
     }
- 
+
     public static PinFunction fromCode(String code) {
         if (code != null) {
             for (PinFunction g : PinFunction.values()) {
@@ -44,5 +46,5 @@ public enum PinFunction {
         }
         return null;
     }
-    
+
 }
--- src/main/java/com/maehem/mangocad/model/element/enums/PinVisible.java
+++ src/main/java/com/maehem/mangocad/model/element/enums/PinVisible.java
@@ -1,38 +1,40 @@
 /*
-    Licensed to the Apache Software Foundation (ASF) under one or more 
+    Licensed to the Apache Software Foundation (ASF) under one or more
     contributor license agreements.  See the NOTICE file distributed with this
-    work for additional information regarding copyright ownership.  The ASF 
-    licenses this file to you under the Apache License, Version 2.0 
-    (the "License"); you may not use this file except in compliance with the 
+    work for additional information regarding copyright ownership.  The ASF
+    licenses this file to you under the Apache License, Version 2.0
+    (the "License"); you may not use this file except in compliance with the
     License.  You may obtain a copy of the License at
 
       http://www.apache.org/licenses/LICENSE-2.0
 
-    Unless required by applicable law or agreed to in writing, software 
-    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT 
-    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the 
-    License for the specific language governing permissions and limitations 
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+    License for the specific language governing permissions and limitations
     under the License.
  */
 package com.maehem.mangocad.model.element.enums;
 
 /**
  * PinVisible        off | pad | pin | both
- * 
+ *
  * @author Mark J Koch ( @maehem on GitHub)
  */
 public enum PinVisible {
     OFF("off"), PAD("pad"), PIN("pin"), BOTH("both");
     private final String code;
- 
+
+    public static final String COMMAND_KEY = "visible";
+
     private PinVisible(String code) {
         this.code = code;
     }
- 
+
     public String code() {
         return code;
     }
- 
+
     public static PinVisible fromCode(String code) {
         if (code != null) {
             for (PinVisible g : PinVisible.values()) {
@@ -43,5 +45,5 @@ public enum PinVisible {
         }
         return null;
     }
-    
+
 }
