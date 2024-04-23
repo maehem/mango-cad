a9fac4e3ab848f841ccd09dcf88043253e50e33e
Mark J Koch <maehem@users.noreply.github.com>
2024-04-23 08:44:31 -0700

    Pin class field identifier for listener service.

---
+++ src/main/java/com/maehem/mangocad/model/element/enums/PinField.java
@@ -0,0 +1,50 @@
+/*
+    Licensed to the Apache Software Foundation (ASF) under one or more
+    contributor license agreements.  See the NOTICE file distributed with this
+    work for additional information regarding copyright ownership.  The ASF
+    licenses this file to you under the Apache License, Version 2.0
+    (the "License"); you may not use this file except in compliance with the
+    License.  You may obtain a copy of the License at
+
+      http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+    License for the specific language governing permissions and limitations
+    under the License.
+ */
+package com.maehem.mangocad.model.element.enums;
+
+/**
+ *
+ * @author Mark J Koch ( @maehem on GitHub )
+ */
+public enum PinField {
+    NAME("name", String.class),
+    X("x", Double.class), Y("y", Double.class),
+    VISIBLE("visible", PinVisible.class),
+    LENGTH("length", PinLength.class),
+    DIRECTION("direction", PinDirection.class),
+    FUNCTION("function", PinFunction.class),
+    SWAPLEVEL("swapLevel", Integer.class),
+    ROTATION("rotation", Double.class),
+    PAD_VALUE("padValue", String.class);
+
+    private final String fName;
+    private final Class clazz;
+
+    private PinField(String name, Class clazz) {
+        this.fName = name;
+        this.clazz = clazz;
+    }
+
+    public String fName() {
+        return fName;
+    }
+
+    public Class clazz() {
+        return clazz;
+    }
+
+}
