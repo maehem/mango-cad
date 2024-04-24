992de9aeb696b5f130c2fa853fbbfc95efaf7cfe
Mark J Koch <maehem@users.noreply.github.com>
2024-04-23 23:27:37 -0700

    Add notifier for field value changes.

--- src/main/java/com/maehem/mangocad/model/element/basic/ElementCircle.java
+++ src/main/java/com/maehem/mangocad/model/element/basic/ElementCircle.java
@@ -1,22 +1,23 @@
 /*
-    Licensed to the Apache Software Foundation (ASF) under one or more 
+    Licensed to the Apache Software Foundation (ASF) under one or more
     contributor license agreements.  See the NOTICE file distributed with this
-    work for additional information regarding copyright ownership.  The ASF 
-    licenses this file to you under the Apache License, Version 2.0 
-    (the "License"); you may not use this file except in compliance with the 
+    work for additional information regarding copyright ownership.  The ASF
+    licenses this file to you under the Apache License, Version 2.0
+    (the "License"); you may not use this file except in compliance with the
     License.  You may obtain a copy of the License at
 
       http://www.apache.org/licenses/LICENSE-2.0
 
-    Unless required by applicable law or agreed to in writing, software 
-    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT 
-    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the 
-    License for the specific language governing permissions and limitations 
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+    License for the specific language governing permissions and limitations
     under the License.
  */
 package com.maehem.mangocad.model.element.basic;
 
 import com.maehem.mangocad.model.Element;
+import com.maehem.mangocad.model.element.enums.ElementCircleField;
 import java.util.ArrayList;
 
 /**
@@ -48,7 +49,9 @@ public class ElementCircle extends Element {
      * @param radius the radius to set
      */
     public void setRadius(double radius) {
+        double oldValue = this.radius;
         this.radius = radius;
+        notifyListeners(ElementCircleField.RADIUS, oldValue, this.radius);
     }
 
     /**
@@ -62,7 +65,9 @@ public class ElementCircle extends Element {
      * @param width the width to set
      */
     public void setWidth(double width) {
+        double oldValue = this.width;
         this.width = width;
+        notifyListeners(ElementCircleField.WIDTH, oldValue, this.width);
     }
 
     /**
@@ -76,7 +81,9 @@ public class ElementCircle extends Element {
      * @param x the x to set
      */
     public void setX(double x) {
+        double oldValue = this.x;
         this.x = x;
+        notifyListeners(ElementCircleField.X, oldValue, this.x);
     }
 
     /**
@@ -90,9 +97,11 @@ public class ElementCircle extends Element {
      * @param y the y to set
      */
     public void setY(double y) {
+        double oldValue = this.y;
         this.y = y;
+        notifyListeners(ElementCircleField.Y, oldValue, this.y);
     }
-    
+
     /**
      * @return the grouprefs
      */
