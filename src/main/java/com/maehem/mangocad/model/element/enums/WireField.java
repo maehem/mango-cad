fe50a1294b364740cdf9d020abf466ea267ae9e6
Mark J Koch <maehem@users.noreply.github.com>
2024-05-09 10:31:30 -0700

    Add "End" field to unum list.

--- src/main/java/com/maehem/mangocad/model/element/enums/WireField.java
+++ src/main/java/com/maehem/mangocad/model/element/enums/WireField.java
@@ -23,6 +23,7 @@ package com.maehem.mangocad.model.element.enums;
 public enum WireField {
     X1("x1", Double.class), Y1("y1", Double.class),
     X2("x2", Double.class), Y2("y2", Double.class),
+    END("selectedEnd", WireEnd.class),
     WIDTH("width", Double.class),
     LAYER("layer", Integer.class),
     EXTENT("extent", String.class),
