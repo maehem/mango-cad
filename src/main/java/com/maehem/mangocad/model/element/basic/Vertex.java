32e0ebfa21a4361dbbdf95fbcce15f320888f8b1
Mark J Koch <maehem@users.noreply.github.com>
2024-05-19 09:16:15 -0700

    Implement ElementXY and ElementSelectable

--- src/main/java/com/maehem/mangocad/model/element/basic/Vertex.java
+++ src/main/java/com/maehem/mangocad/model/element/basic/Vertex.java
@@ -1,41 +1,54 @@
 /*
-    Licensed to the Apache Software Foundation (ASF) under one or more 
+    Licensed to the Apache Software Foundation (ASF) under one or more
     contributor license agreements.  See the NOTICE file distributed with this
-    work for additional information regarding copyright ownership.  The ASF 
-    licenses this file to you under the Apache License, Version 2.0 
-    (the "License"); you may not use this file except in compliance with the 
+    work for additional information regarding copyright ownership.  The ASF
+    licenses this file to you under the Apache License, Version 2.0
+    (the "License"); you may not use this file except in compliance with the
     License.  You may obtain a copy of the License at
 
       http://www.apache.org/licenses/LICENSE-2.0
 
-    Unless required by applicable law or agreed to in writing, software 
-    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT 
-    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the 
-    License for the specific language governing permissions and limitations 
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+    License for the specific language governing permissions and limitations
     under the License.
  */
 package com.maehem.mangocad.model.element.basic;
 
 import com.maehem.mangocad.model.Element;
+import com.maehem.mangocad.model.ElementSelectable;
+import com.maehem.mangocad.model.ElementXY;
+import com.maehem.mangocad.model.element.enums.VertexField;
 
 /**
  *
  * @author Mark J Koch ( @maehem on GitHub)
  */
-public class Vertex extends Element {
+public class Vertex extends Element implements ElementXY, ElementSelectable {
+
     public static final String ELEMENT_NAME = "vertex";
 
     private double x;
     private double y;
     private double curve = 0;
+    private boolean selected = false;
+    private final Object parent;
+    private final double[] snapshot = {0, 0};
+
+    public Vertex(Object parent) {
+        this.parent = parent;
+    }
 
     @Override
     public String getElementName() {
         return ELEMENT_NAME;
     }
+
     /**
      * @return the x
      */
+    @Override
     public double getX() {
         return x;
     }
@@ -43,13 +56,19 @@ public class Vertex extends Element {
     /**
      * @param x the x to set
      */
+    @Override
     public void setX(double x) {
-        this.x = x;
+        if (this.x != x) {
+            double oldVal = this.x;
+            this.x = x;
+            notifyListeners(VertexField.X, oldVal, this.x);
+        }
     }
 
     /**
      * @return the y
      */
+    @Override
     public double getY() {
         return y;
     }
@@ -57,8 +76,13 @@ public class Vertex extends Element {
     /**
      * @param y the y to set
      */
+    @Override
     public void setY(double y) {
-        this.y = y;
+        if (this.y != y) {
+            double oldVal = this.y;
+            this.y = y;
+            notifyListeners(VertexField.Y, oldVal, this.y);
+        }
     }
 
     /**
@@ -73,7 +97,48 @@ public class Vertex extends Element {
      */
     public void setCurve(double curve) {
         this.curve = curve;
+        if (this.curve != curve) {
+            double oldVal = this.curve;
+            this.curve = curve;
+            notifyListeners(VertexField.CURVE, oldVal, this.curve);
+        }
     }
 
+    /**
+     * @return the selected
+     */
+    @Override
+    public boolean isSelected() {
+        return selected;
+    }
+
+    /**
+     * @param selected the selected to set
+     */
+    @Override
+    public void setSelected(boolean selected) {
+        if (this.selected != selected) {
+            boolean oldValue = this.selected;
+            this.selected = selected;
+            notifyListeners(VertexField.SELECTED, oldValue, this.selected);
+        }
+    }
+
+    @Override
+    public void createSnapshot() {
+        snapshot[0] = getX();
+        snapshot[1] = getY();
+    }
+
+    @Override
+    public void restoreSnapshot() {
+        setX(snapshot[0]);
+        setY(snapshot[1]);
+    }
+
+    @Override
+    public double[] getSnapshot() {
+        return snapshot;
+    }
 
 }
