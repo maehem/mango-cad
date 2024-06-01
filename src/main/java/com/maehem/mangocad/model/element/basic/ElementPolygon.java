c330f5c37d8d7a97c909642302482863e99de008
Mark J Koch <maehem@users.noreply.github.com>
2024-06-01 11:07:13 -0700

    addVertex() methods and set a default width.

--- src/main/java/com/maehem/mangocad/model/element/basic/ElementPolygon.java
+++ src/main/java/com/maehem/mangocad/model/element/basic/ElementPolygon.java
@@ -20,19 +20,25 @@
 package com.maehem.mangocad.model.element.basic;
 
 import com.maehem.mangocad.model.Element;
+import com.maehem.mangocad.model.ElementListener;
+import com.maehem.mangocad.model.ElementSelectable;
 import com.maehem.mangocad.model.FieldWidth;
+import com.maehem.mangocad.model.element.enums.ElementPolygonField;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
  * @author Mark J Koch ( @maehem on GitHub)
  */
-public class ElementPolygon extends Element implements FieldWidth {
+public class ElementPolygon extends Element implements ElementListener, ElementSelectable, FieldWidth {
 
+    public static final Logger LOGGER = Logger.getLogger("com.maehem.mangocad");
     public static final String ELEMENT_NAME = "polygon";
 
-    private double width;
+    private double width = 0.254;
 //    private double spacing;
 //    private String pour = "solid"; // TODO: Enum
     //private double isolate;
@@ -40,6 +46,10 @@ public class ElementPolygon extends Element implements FieldWidth {
     //private boolean thermals = true;
     //private int rank = 1;
     private final List<Vertex> vertices = new ArrayList<>();
+
+    // Ephemaral data
+    private ElementPolygon snapshot = null;
+    private boolean selected = false;
 
     @Override
     public String getElementName() {
@@ -152,7 +162,6 @@ public class ElementPolygon extends Element implements FieldWidth {
 //            this.rank = rank;
 //        }
 //    }
-
     /**
      * @return the vertices
      */
@@ -167,6 +176,56 @@ public class ElementPolygon extends Element implements FieldWidth {
         this.vertices.clear();
         for (Vertex v : vertices) {
             this.vertices.add(v);
+        }
+    }
+
+    /**
+     * Add a new vertex at the end of the list.
+     *
+     * @param vertNew
+     */
+    public void addVertex(Vertex vertNew) {
+        LOGGER.log(Level.SEVERE, "Polygon added a vertex. {0},{1}  obj:{2}", new Object[]{vertNew.getX(), vertNew.getY(), vertNew.hashCode()});
+        getVertices().add(vertNew);
+        StringBuilder sb = new StringBuilder("PolyList:");
+        for (Vertex v : getVertices()) {
+            sb.append("\n    ");
+            sb.append(v.hashCode());
+            if (getVertices().getFirst().equals(v)) {
+                sb.append("(*)");
+            }
+            sb.append(" ===>  ");
+            sb.append("x: ");
+            sb.append(v.getX());
+            sb.append(",  y: ");
+            sb.append(v.getY());
+        }
+        LOGGER.log(Level.SEVERE, sb.toString());
+        vertNew.addListener(this);
+        notifyListeners(ElementPolygonField.VERTEX, null, vertNew);
+    }
+
+    /**
+     * Insert a Vertex in the list, after the specified vertex.
+     *
+     * @param before adds @Vertex after this one.
+     * @param vertNew the new @Vertex to be added.
+     */
+    public void addVertex(Vertex before, Vertex vertNew) {
+        LOGGER.log(Level.SEVERE, "Polygon added a vertex. {0},{1}", new Object[]{vertNew.getX(), vertNew.getY()});
+        List<Vertex> verts = getVertices();
+        getVertices().add(verts.indexOf(before) + 1, vertNew);
+
+        vertNew.addListener(this);
+        notifyListeners(ElementPolygonField.VERTEX, null, vertNew);
+    }
+
+    public void removeVertex(Vertex v) {
+        LOGGER.log(Level.SEVERE, "Polygon removed a vertex. {0},{1}", new Object[]{v.getX(), v.getY()});
+        if (getVertices().contains(v)) {
+            getVertices().remove(v);
+            v.removeListener(this);
+            notifyListeners(ElementPolygonField.VERTEX, v, null);
         }
     }
 
@@ -194,7 +253,7 @@ public class ElementPolygon extends Element implements FieldWidth {
     }
 
     /**
-     * Return a list of vertices that are selected.     * given X/Y.
+     * Return a list of vertices that are selected. * given X/Y.
      *
      * @param x
      * @param y
@@ -212,4 +271,58 @@ public class ElementPolygon extends Element implements FieldWidth {
 
         return found.toArray(new Vertex[]{});
     }
+
+    public ElementPolygon copy() {
+        ElementPolygon copy = new ElementPolygon();
+
+        copy.setWidth(getWidth());
+        copy.setLayer(getLayerNum());
+        copy.setVertices(getVertices());
+
+        return copy;
+    }
+
+    @Override
+    public void createSnapshot() {
+        snapshot = copy();
+    }
+
+    @Override
+    public void restoreSnapshot() {
+        if (snapshot != null) {
+            setWidth(snapshot.getWidth());
+            setLayer(snapshot.getLayerNum());
+            setVertices(snapshot.getVertices());
+
+            snapshot = null;
+        }
+    }
+
+    @Override
+    public Element getSnapshot() {
+        if (snapshot == null) {
+            createSnapshot();
+        }
+        return snapshot;
+    }
+
+    @Override
+    public boolean isSelected() {
+        return selected;
+    }
+
+    @Override
+    public void setSelected(boolean selected) {
+        if (this.selected != selected) {
+            boolean oldValue = this.selected;
+            this.selected = selected;
+            notifyListeners(ElementPolygonField.SELECTED, oldValue, this.selected);
+        }
+    }
+
+    @Override
+    public void elementChanged(Element e, Enum field, Object oldVal, Object newVal) {
+        notifyListeners(field, oldVal, newVal);
+    }
+
 }
