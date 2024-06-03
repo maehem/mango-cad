98684225e562e8b17ed8ba84f810306bffc4669c
Mark J Koch <maehem@users.noreply.github.com>
2024-06-02 22:51:34 -0700

    Reformat code. Remove unused import.

--- src/main/java/com/maehem/mangocad/model/element/basic/ModuleInst.java
+++ src/main/java/com/maehem/mangocad/model/element/basic/ModuleInst.java
@@ -1,17 +1,17 @@
 /*
-    Licensed to the Apache Software Foundation (ASF) under one or more 
+    Licensed to the Apache Software Foundation (ASF) under one or more
     contributor license agreements.  See the NOTICE file distributed with this
-    work for additional information regarding copyright ownership.  The ASF 
-    licenses this file to you under the Apache License, Version 2.0 
-    (the "License"); you may not use this file except in compliance with the 
+    work for additional information regarding copyright ownership.  The ASF
+    licenses this file to you under the Apache License, Version 2.0
+    (the "License"); you may not use this file except in compliance with the
     License.  You may obtain a copy of the License at
 
       http://www.apache.org/licenses/LICENSE-2.0
 
-    Unless required by applicable law or agreed to in writing, software 
-    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT 
-    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the 
-    License for the specific language governing permissions and limitations 
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+    License for the specific language governing permissions and limitations
     under the License.
  */
 package com.maehem.mangocad.model.element.basic;
@@ -20,7 +20,6 @@ import com.maehem.mangocad.model.Element;
 import com.maehem.mangocad.model.util.Rotation;
 import java.util.ArrayList;
 import java.util.List;
-import javafx.scene.robot.Robot;
 
 /**
      * moduleinst ( attribute(s) )
@@ -34,15 +33,15 @@ import javafx.scene.robot.Robot;
           rot           %Rotation;     "R0"
           >
           <!-- rot: Only 0, 90, 180 or 270 -->
- * 
+ *
  * @author Mark J Koch ( @maehem on GitHub )
  */
 public class ModuleInst extends Element {
-    
+
     public static final String ELEMENT_NAME = "moduleinst";
-    
+
     private final ArrayList<Attribute> attributes = new ArrayList<>();
-    
+
     private String name;
     private String module;
     private String moduleVariant = "";
@@ -130,7 +129,7 @@ public class ModuleInst extends Element {
     public Rotation getRotation() {
         return rotation;
     }
-    
+
     /**
      * @return the rot
      */
