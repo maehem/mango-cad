495ffab753f6e37fa2647b59382eebb89f897c76
Mark J Koch <maehem@users.noreply.github.com>
2024-06-18 22:09:07 -0700

    Reformat. Add toXML() method.

--- src/main/java/com/maehem/mangocad/model/element/misc/Description.java
+++ src/main/java/com/maehem/mangocad/model/element/misc/Description.java
@@ -1,22 +1,23 @@
 /*
-    Licensed to the Apache Software Foundation (ASF) under one or more 
+    Licensed to the Apache Software Foundation (ASF) under one or more
     contributor license agreements.  See the NOTICE file distributed with this
-    work for additional information regarding copyright ownership.  The ASF 
-    licenses this file to you under the Apache License, Version 2.0 
-    (the "License"); you may not use this file except in compliance with the 
+    work for additional information regarding copyright ownership.  The ASF
+    licenses this file to you under the Apache License, Version 2.0
+    (the "License"); you may not use this file except in compliance with the
     License.  You may obtain a copy of the License at
 
       http://www.apache.org/licenses/LICENSE-2.0
 
-    Unless required by applicable law or agreed to in writing, software 
-    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT 
-    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the 
-    License for the specific language governing permissions and limitations 
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+    License for the specific language governing permissions and limitations
     under the License.
  */
 package com.maehem.mangocad.model.element.misc;
 
 import com.maehem.mangocad.model.Element;
+import java.text.MessageFormat;
 
 /**
  *
@@ -24,13 +25,13 @@ import com.maehem.mangocad.model.Element;
  */
 public class Description extends Element {
     public static final String ELEMENT_NAME = "description";
-    
+
     private String locale = "us_EN"; // The i18n locale code for this text. No value = en_US
     private String value = "";  // The text
 
     public Description() {
     }
-    
+
     public Description(String value) {
         setValue(value);
     }
@@ -39,13 +40,13 @@ public class Description extends Element {
         this(value);
         setLocale(locale);
     }
-    
+
 
     @Override
     public String getElementName() {
         return ELEMENT_NAME;
     }
-    
+
     /**
      * @return the locale
      */
@@ -73,4 +74,16 @@ public class Description extends Element {
     public final void setValue(String value) {
         this.value = value;
     }
+
+    @Override
+    public String toXML() {
+        MessageFormat mf = new MessageFormat("<description>{0}</description>");
+
+        Object[] args = {
+            escapeHTML(value), // 0
+        };
+
+        return mf.format(args);
+    }
+
 }
