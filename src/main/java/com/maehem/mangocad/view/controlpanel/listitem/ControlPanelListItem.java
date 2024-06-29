79c821f776cb1068f4befd9e0ac03dfbbc95fdd5
Mark J Koch <maehem@users.noreply.github.com>
2024-06-29 15:36:50 -0700

    Reformat and remove old code.

--- src/main/java/com/maehem/mangocad/view/controlpanel/listitem/ControlPanelListItem.java
+++ src/main/java/com/maehem/mangocad/view/controlpanel/listitem/ControlPanelListItem.java
@@ -1,17 +1,17 @@
 /*
-    Licensed to the Apache Software Foundation (ASF) under one or more 
+    Licensed to the Apache Software Foundation (ASF) under one or more
     contributor license agreements.  See the NOTICE file distributed with this
-    work for additional information regarding copyright ownership.  The ASF 
-    licenses this file to you under the Apache License, Version 2.0 
-    (the "License"); you may not use this file except in compliance with the 
+    work for additional information regarding copyright ownership.  The ASF
+    licenses this file to you under the Apache License, Version 2.0
+    (the "License"); you may not use this file except in compliance with the
     License.  You may obtain a copy of the License at
 
       http://www.apache.org/licenses/LICENSE-2.0
 
-    Unless required by applicable law or agreed to in writing, software 
-    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT 
-    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the 
-    License for the specific language governing permissions and limitations 
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+    License for the specific language governing permissions and limitations
     under the License.
  */
 package com.maehem.mangocad.view.controlpanel.listitem;
@@ -125,21 +125,11 @@ public abstract class ControlPanelListItem {
         } else {
             return null;
         }
-
-//        
-//        
-//        
-//        if ( getDescription().isEmpty() ) {
-//            return null;
-//        }
-//        Tooltip tt = new Tooltip();
-//        tt.setGraphic(ControlPanelUtils.markdownNode(getDescription()));
-//        return tt;
     }
 
     public Node getPreviewTabNode() {
         String path = getFile().toPath().toUri().toString();
-        ScrollPane sp = new ScrollPane(MarkdownUtils.markdownNode(1, 
+        ScrollPane sp = new ScrollPane(MarkdownUtils.markdownNode(1,
                 ControlPanelUtils.getItemDescriptionFull(this),path
         ));
         sp.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
